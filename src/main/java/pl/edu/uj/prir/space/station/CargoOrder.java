f021b5ee774a695f783c264a28b43cd8cf16be02
andrzej <michal.jazowski@student.uj.edu.pl>
2017-11-12 09:39:39 +0100

    Dodałem metody pomocnicze CargoOrder i licznik wystąpień

--- src/main/java/pl/edu/uj/prir/space/station/CargoOrder.java
+++ src/main/java/pl/edu/uj/prir/space/station/CargoOrder.java
@@ -6,12 +6,27 @@ package pl.edu.uj.prir.space.station;
  * @author michal jazowski on 11.11.17.
  */
 public class CargoOrder {
+    private static int counter = 0;
     private final CargoInterface cargo;
     private final Direction direction;
 
     public CargoOrder(CargoInterface cargo, Direction direction) {
         this.cargo = cargo;
         this.direction = direction;
+        ++counter;
+    }
+
+    public CargoOrder(CargoOrder cargoOrder) {
+        this(cargoOrder.getCargo(), cargoOrder.getDirection());
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Cargo[id: %d, size: %d, direction: %s]",
+                counter,
+                getSize(),
+                cargo.getDirection());
     }
 
     public CargoInterface getCargo() {
@@ -24,5 +39,9 @@ public class CargoOrder {
 
     public boolean fromInside() {
         return direction == Direction.OUTSIDE;
+    }
+
+    private Direction getDirection() {
+        return direction;
     }
 }
