b763452210a50688b5a2f968c2f3d44a1b9993aa
andrzej <michal.jazowski@student.uj.edu.pl>
2017-11-12 09:56:29 +0100

    Dodałem test MoonBase

---
+++ src/test/java/pl/edu/uj/prir/space/station/MoonBaseTest.java
@@ -0,0 +1,15 @@
+package pl.edu.uj.prir.space.station;
+
+import org.testng.annotations.Test;
+
+/**
+ * Copyright: Format C
+ *
+ * @author michal jazowski on 12.11.17.
+ */
+public class MoonBaseTest {
+    @Test
+    public void moonBaseTest() {
+        
+    }
+}
